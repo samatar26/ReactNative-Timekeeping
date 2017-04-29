a883fc3542725ee6296e430eb0702a94d267ca63
Samatar Axmed <Samatar_267@hotmail.com>
2017-04-29 16:17:44 +0100

    Added three buttons The buttons will provide a link to different forms/lists

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,48 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 export default class Footer extends Component {
   render() {
     return (
-      <View />
+      <View style={styles.container}>
+        <View style={styles.menu}>
+          <TouchableOpacity style={styles.menuItem}>
+            <Text style={styles.text}>Klant</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.menuItem}>
+            <Text style={styles.text}>Project</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.menuItem}>
+            <Text style={styles.text}>Tijdsduur</Text>
+          </TouchableOpacity>
+        </View>
+        </View>
     );
   }
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  menu: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent:"space-between",
+    width:"100%"
+  },
+  menuItem: {
+    padding: 8,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "rgb(24, 60, 18)",
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "800"
+  }
+});
