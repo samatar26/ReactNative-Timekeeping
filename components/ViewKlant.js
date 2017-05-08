e8ff26b1c3c7f5a28c47e81c2db31f962b59bc0f
Samatar Axmed <Samatar_267@hotmail.com>
2017-05-08 21:18:06 +0100

    ViewKlant displays data from database The component ViewKlant is now able to display data from the database, next step is to display all the data, instead of just the first name and also add the ability to delete the client from the database

--- components/ViewKlant.js
+++ components/ViewKlant.js
@@ -3,7 +3,11 @@ import {View, Text, StyleSheet} from 'react-native';
 import realm from './../database/Realm';
 
 export const ViewKlant = () => (
-  <Text>Yes</Text>
+  <View style={styles.picker}>
+    {Array.from(realm.objects('Klant')).map((klant)=>{
+      return <Text>{klant.voornaam}</Text>;
+    })}
+  </View>
 
 );
 
